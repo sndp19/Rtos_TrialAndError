cefef0bfedbf7b8bf6cc2e3adc5aad251a715be0
Sandeep Varma <sandeepvarma1929@gmail.com>
2024-08-15 16:56:03 +0530

    Created Project and Added ThirdParty

---
+++ Rtos_TrialAndError/Rtos_TrialAndError/Debug/ThirdParty/FreeRTOS/croutine.d
@@ -0,0 +1,21 @@
+ThirdParty/FreeRTOS/croutine.o: ../ThirdParty/FreeRTOS/croutine.c \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/projdefs.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/portable.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/task.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/list.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/croutine.h
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/projdefs.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/portable.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/task.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/list.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/croutine.h:
---
+++ Rtos_TrialAndError/Rtos_TrialAndError/Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/projdefs.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/portable.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/task.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/list.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/timers.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/task.h \
+ E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/stack_macros.h
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/projdefs.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/portable.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/task.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/list.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/timers.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/task.h:
+E:/02Programming_folder/02.2.\ Microcontroller/02_RTOS_Workspace/001_rtos_TrialError/ThirdParty/FreeRTOS/include/stack_macros.h:
